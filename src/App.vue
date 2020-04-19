15c1a4e0414539d0b4fb85cbb697e41f55782463
Arsam Sarabi <arsamsarabi@me.com>
2020-04-20 00:00:17 +0100

    ✨ fetches info and displays, need styling

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,115 @@
 <template>
-  <div id="app">
-    <h1>Hello World!</h1>
+  <div
+    id="app"
+    :class="typeof weather.main !== 'undefined' ? (weather.main.temp > 16 ? 'warm' : 'cold') : ''"
+  >
+    <main>
+      <div class="search-box">
+        <input
+          type="text"
+          placeholder="Search..."
+          class="input-field"
+          v-model="location"
+          @keyup.enter="fetchWeather"
+        />
+      </div>
+
+      <section class="weather-results" v-if="typeof weather.main !== 'undefined'">
+        <div class="location-details">
+          <div class="city">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ datetime }}</div>
+        </div>
+        <div class="wheater-info">
+          <div class="temperature">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="condition">{{ weather.weather[0].main }}</div>
+          <div class="condition-desc">{{ weather.weather[0].description }}</div>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
+import dayjs from 'dayjs'
+
 export default {
   name: 'App',
   data() {
     return {
-      API_KEY: process.env.VUE_APP_API_KEY,
+      location: '',
+      unit: 'metric',
+      datetime: '',
+      weather: {},
     }
+  },
+  methods: {
+    fetchWeather() {
+      const fetchUrl = `${process.env.VUE_APP_API_BASE_URL}weather?q=${this.location}&units=${this.unit}&APPID=${process.env.VUE_APP_API_KEY}`
+      fetch(fetchUrl)
+        .then(res => res.json())
+        .then(this.handleResult)
+    },
+    handleResult(weather) {
+      this.weather = weather
+      this.dateBuilder()
+    },
+    dateBuilder() {
+      this.datetime = dayjs()
+        .add(this.weather.timezone, 'second')
+        .format('DD MMM YYYY')
+    },
   },
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+#app {
+  background-image: url(./assets/images/bg.svg);
+  background-size: 350% 100%;
+  background-position: -550px;
+  background-repeat: no-repeat;
+}
+
+#app.warm {
+  background-image: url(./assets/images/bg_day.svg);
+  background-position: center center;
+}
+
+#app.cold {
+  background-image: url(./assets/images/bg_night.svg);
+  background-position: center center;
+}
+
+main {
+  width: 100%;
+  height: 100vh;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+  padding: 16px;
+}
+
+.input-field {
+  width: 100%;
+  height: 48px;
+  border: none;
+  outline: none;
+  background: none;
+  background-color: rgba(0, 0, 0, 0.25);
+  padding: 8px 16px;
+  font-size: 1.25rem;
+  color: #f9f9f9;
+  border-radius: 8px 0 8px 0;
+  transition: 0.4s all ease-out;
+
+  &:focus,
+  &:active {
+    outline: none;
+    background-color: rgba(0, 0, 0, 0.75);
+    border-radius: 0 8px 0 8px;
+  }
+}
+
+::placeholder {
+  color: #f1f1f1;
+  opacity: 1;
+}
+</style>
