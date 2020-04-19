4cd480d73e688804b74832ed8f62af952c47bf3c
Arsam Sarabi <arsamsarabi@me.com>
2020-04-19 16:51:35 +0100

    💄 global styles added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <h1>Hello World!</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "App",
+  name: 'App',
   data() {
     return {
-      API_KEY: process.env.VUE_APP_API_KEY
-    };
-  }
-};
+      API_KEY: process.env.VUE_APP_API_KEY,
+    }
+  },
+}
 </script>
 
 <style lang="scss"></style>
