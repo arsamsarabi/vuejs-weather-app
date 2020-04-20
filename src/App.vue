54775d9bddaeb07b0e913adadb098a9b66c6ed41
Arsam Sarabi <arsamsarabi@me.com>
2020-04-20 18:09:49 +0100

    ✨ <WeatherForm />

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div
     id="app"
-    :class="typeof weather.main !== 'undefined' ? (weather.main.temp > 16 ? 'warm' : 'cold') : ''"
+    :class="[
+      { warm: typeof weather.main !== 'undefined' && weather.main.temp > 16 },
+      { cold: typeof weather.main !== 'undefined' && weather.main.temp < 16 },
+    ]"
   >
     <main>
-      <div class="search-box">
-        <input
-          type="text"
-          placeholder="Search..."
-          class="input-field"
-          v-model="location"
-          @keyup.enter="fetchWeather"
-        />
-      </div>
-
+      <WeatherForm v-on:handle-search="fetchWeather" />
       <WeatherInformation :weather="weather" :datetime="datetime" />
     </main>
   </div>
@@ -23,33 +17,32 @@
 import dayjs from 'dayjs'
 
 import WeatherInformation from './components/WeatherInformation'
+import WeatherForm from './components/WeatherForm'
 
 export default {
   name: 'App',
   components: {
     WeatherInformation,
+    WeatherForm,
   },
   data() {
     return {
-      location: '',
-      unit: 'metric',
-      datetime: '',
       weather: {},
     }
   },
   methods: {
-    fetchWeather() {
-      const fetchUrl = `${process.env.VUE_APP_API_BASE_URL}weather?q=${this.location}&units=${this.unit}&APPID=${process.env.VUE_APP_API_KEY}`
+    fetchWeather(location, unit) {
+      const fetchUrl = `${process.env.VUE_APP_API_BASE_URL}weather?q=${location}&units=${unit}&APPID=${process.env.VUE_APP_API_KEY}`
       fetch(fetchUrl)
         .then(res => res.json())
-        .then(this.handleResult)
+        .then(data => {
+          this.weather = data
+        })
     },
-    handleResult(weather) {
-      this.weather = weather
-      this.dateBuilder()
-    },
-    dateBuilder() {
-      this.datetime = dayjs()
+  },
+  computed: {
+    datetime: function() {
+      return dayjs()
         .add(this.weather.timezone, 'second')
         .format('DD MMM YYYY')
     },
@@ -59,6 +52,9 @@ export default {
 
 <style lang="scss">
 #app {
+  width: 375px;
+  height: 812px;
+  overflow: hidden;
   background-image: url(./assets/images/bg.svg);
   background-size: 350% 100%;
   background-position: -550px;
@@ -81,31 +77,5 @@ main {
   height: 100vh;
   background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
   padding: 16px;
-}
-
-.input-field {
-  width: 100%;
-  height: 48px;
-  border: none;
-  outline: none;
-  background: none;
-  background-color: rgba(0, 0, 0, 0.25);
-  padding: 8px 16px;
-  font-size: 1.25rem;
-  color: var(--color-white);
-  border-radius: 8px 0 8px 0;
-  transition: 0.4s all ease-out;
-
-  &:focus,
-  &:active {
-    outline: none;
-    background-color: rgba(0, 0, 0, 0.75);
-    border-radius: 0 8px 0 8px;
-  }
-}
-
-::placeholder {
-  color: var(--color-white);
-  opacity: 1;
 }
 </style>
---
+++ src/components/WeatherForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="search-box">
+    <input
+      type="text"
+      placeholder="Search..."
+      class="input-field"
+      v-model="location"
+      @keyup.enter="$emit('handle-search', location, unit)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeatherForm',
+  data() {
+    return {
+      location: '',
+      unit: 'metric',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.input-field {
+  width: 100%;
+  height: 48px;
+  border: none;
+  outline: none;
+  background: none;
+  background-color: rgba(0, 0, 0, 0.25);
+  padding: 8px 16px;
+  font-size: 1.25rem;
+  color: var(--color-white);
+  border-radius: 8px 0 8px 0;
+  transition: 0.4s all ease-out;
+
+  &:focus,
+  &:active {
+    outline: none;
+    background-color: rgba(0, 0, 0, 0.75);
+    border-radius: 0 8px 0 8px;
+  }
+}
+
+::placeholder {
+  color: var(--color-white);
+  opacity: 1;
+}
+</style>
